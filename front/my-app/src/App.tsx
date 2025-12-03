068a30232bc1b1e0e217269356d40828d5846e83
DoubleAMjunior <aliabbasi1377@gmail.com>
2025-12-03 20:34:29 +0330

    add loading for the front end

--- front/my-app/src/App.tsx
+++ front/my-app/src/App.tsx
@@ -1,20 +1,24 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import IssueTracker from './Components/IssueTracker';
 import { useIssueStore } from './stores';
 
 function App() {
   const fetchIssues = useIssueStore((state) => state.fetchIssues);
+  const [hasLoaded,setHasLoaded]=useState(false)
   useEffect(() => {
   fetchIssues((success, message) => {
     if (!success) alert("Error fetching issues: " + message);
+    setHasLoaded(true)
   });
 }, [fetchIssues]);
-  return (
-     <div style={{ maxWidth: 600, margin: "0 auto", padding: 20 }}>
+  return (<>
+     {!hasLoaded? <h1 style={{ textAlign: "center" }}>Loading</h1>:(
+      <div style={{ maxWidth: 600, margin: "0 auto", padding: 20 }}>
       <h2>Issue Tracker</h2>
-      <IssueTracker></IssueTracker>
-    </div>
+      <IssueTracker/>
+    </div>)}
+    </>
   );
 }
 
